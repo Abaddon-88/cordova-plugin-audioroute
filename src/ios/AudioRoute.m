815a1fc6ad2ca13d5c2e994d2c8ba09976fc8701
Bastian Göbet <bastian.goebet@webfactor.de>
2019-03-06 13:39:29 +0100

    add setAudio Mode

--- src/ios/AudioRoute.m
+++ src/ios/AudioRoute.m
@@ -171,6 +171,25 @@ NSUInteger _originalOptionsFlag;
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
+- (void)setAudioMode:(CDVInvokedUrlCommand *)command
+{
+    NSError* __autoreleasing err = nil;
+    NSString* mode = [NSString stringWithFormat:@"%@", [command.arguments objectAtIndex:0]];
+    
+    UInt32 audioRouteOverride = kAudioSessionOverrideAudioRoute_None;
+    AVAudioSession *session = [AVAudioSession sharedInstance];
+    
+    if ([mode isEqualToString:@"earpiece"]) {
+        [session setCategory:AVAudioSessionCategoryPlayAndRecord error:&err];
+        audioRouteOverride = kAudioSessionProperty_OverrideCategoryDefaultToSpeaker;
+        AudioSessionSetProperty(kAudioSessionProperty_OverrideAudioRoute, sizeof(audioRouteOverride), &audioRouteOverride);
+    } else if ([mode isEqualToString:@"speaker"] || [mode isEqualToString:@"ringtone"]) {
+        [session setCategory:AVAudioSessionCategoryPlayAndRecord withOptions:AVAudioSessionCategoryOptionDefaultToSpeaker error:&err];
+    } else if ([mode isEqualToString:@"normal"]) {
+        [session setCategory:AVAudioSessionCategorySoloAmbient error:&err];
+    }
+}
+
 - (void)start:(CDVInvokedUrlCommand*)command
 {
     NSString* mediaType = [command.arguments objectAtIndex:0];
